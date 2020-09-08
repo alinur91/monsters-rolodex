45caee681c906a846ea0a61694ce5f43599c4073
Alisher Nurlybay <areshil91@gmail.com>
2020-09-08 17:17:32 +0600

    Updates

---
+++ precache-manifest.df8b2a36d4246601a7d22d59c21e1bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef114c618a814da13807540acf663d4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a764e6271b8cd607e58",
+    "url": "/monsters-rolodex/static/css/main.24c39e92.chunk.css"
+  },
+  {
+    "revision": "67e53e8c609cce5adf15",
+    "url": "/monsters-rolodex/static/js/2.fb25bbb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fb25bbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a764e6271b8cd607e58",
+    "url": "/monsters-rolodex/static/js/main.db9fdede.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
